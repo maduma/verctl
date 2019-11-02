24eaab737bec5039c8d08c70331b7077eb154845
maduma <maduma@pt.lu>
2019-11-02 19:06:29 +0100

    .

--- verctl-start.sh
+++ verctl-start.sh
@@ -15,13 +15,13 @@ az container create \
     -g $APP \
     -n $APP \
     -l westeurope \
-    --image httpd \
+    --image gitlab/gitlab-ce \
     --dns-name-label $APP \
-    --ports 80 \
+    --ports 22 80 443 \
     --azure-file-volume-account-name $APP \
     --azure-file-volume-account-key $STORAGE_KEY \
     --azure-file-volume-share-name $APP \
-    --azure-file-volume-mount-path /usr/local/apache2/htdocs/
+    --azure-file-volume-mount-path /var/opt/gitlab
 
 FQDN=$( az container show \
     -g $APP -n $APP --query "ipAddress.fqdn" -o tsv )
