678f03f81d4fd23c2a0eefe07c6b17f33f81261f
maduma <maduma@pt.lu>
2019-11-02 18:57:13 +0100

    .

--- azure-fs-up.sh
+++ azure-fs-up.sh
@@ -8,20 +8,26 @@ set -xe
 APP=verctl
 LOCATION=westeurope
 
+function is_group_exists {
+    az group show --name $APP -o none 2>/dev/null
+}
+
+function is_storage_exists {
+    az storage account show --name $APP -o none 2>/dev/null
+}
+
+function is_share_exists {
+    az storage share show -n verctl --account-name verctl \
+    -o none 2>/dev/null
+}
+
 # Create a resource group
-az group show --name $APP -o none 2>/dev/null || \
-az group create -l $LOCATION -n $APP
+is_group_exists || az group create -l $LOCATION -n $APP
 
 # Create a storage account
-az storage account show --name $APP -o none 2>/dev/null || \
-az storage account create \
-    -g $APP \
-    -l $LOCATION \
-    --kind StorageV2 \
-    --sku Standard_LRS \
-    -n $APP
+is_storage_exists || az storage account create \
+--kind StorageV2 --sku Standard_LRS -g $APP -l $LOCATION -n $APP
 
 # Create the file share
-az storage share show -n verctl --account-name verctl \
-    -o none 2>/dev/null || \
-az storage share create --account-name $APP -n $APP
+is_group_exists || az storage share create \
+--account-name $APP -n $APP
