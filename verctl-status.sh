2a544cf9ab68dc95441598710539efe7b756cc99
maduma <maduma@pt.lu>
2019-11-02 17:57:23 +0100

    .

---
+++ verctl-status.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -xe
+
+APP=verctl
+
+function is_container_exists {
+    az container show -g $APP -n $APP -o none 2>/dev/null 
+}
+
+function container_state {
+    az container show -g verctl -n verctl \
+    --query "containers[0].instanceView.currentState.state" \
+    -o tsv
+}
+
+function container_fqdn {
+    is_container_exists && \
+    az container show -g $APP -n $APP \
+    --query "ipAddress.fqdn" -o tsv
+}
+
+container_state
+container_fqdn
