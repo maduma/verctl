14be90d8a0e637fda48877a8194707f31696ae40
maduma <maduma@pt.lu>
2019-11-02 17:11:32 +0100

    .

--- azure-up.sh
+++ azure-up.sh
@@ -11,7 +11,7 @@ az group create -l $LOCATION -n $APP
 az group show --name $APP -o table
 
 # Create a storage account
-az storage acount show --name $APP -o none 2>/dev/null || \
+az storage account show --name $APP -o none 2>/dev/null || \
 az storage account create \
     -g $APP \
     -l $LOCATION \
