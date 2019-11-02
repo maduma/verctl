de56dca792ce2f2e960f0226000684e00ae75050
maduma <maduma@pt.lu>
2019-11-02 17:30:00 +0100

    .

--- verctl-start.sh
+++ verctl-start.sh
@@ -2,9 +2,15 @@
 
 set -xe
 
+function error {
+    echo "$@"
+    exit 1
+}
+
 APP=verctl
 LOCATION=westeurope
 STORAGE_KEY=$( az storage account keys list -g $APP \
     --account-name $APP --query "[0].value" --output tsv )
 
-az container show -g $APP -n $APP -o none
+az container show -g $APP -n $APP -o none 2>/dev/null || \
+error "Container $APP already running!"
