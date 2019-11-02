630a88ccbeb21faac0ccdecc3649661a40693f6f
maduma <maduma@pt.lu>
2019-11-02 18:09:25 +0100

    .

--- verctl-stop.sh
+++ verctl-stop.sh
@@ -8,25 +8,8 @@ function error {
 }
 
 APP=verctl
-STORAGE_KEY=$( az storage account keys list -g $APP \
-    --account-name $APP --query "[0].value" --output tsv )
 
 az container show -g $APP -n $APP -o none 2>/dev/null && \
-    error "Container $APP already running!"
+    error "Container $APP do not exists!"
 
-az container create \
-    -g $APP \
-    -n $APP \
-    -l westeurope \
-    --image httpd \
-    --dns-name-label $APP \
-    --ports 80 \
-    --azure-file-volume-account-name $APP \
-    --azure-file-volume-account-key $STORAGE_KEY \
-    --azure-file-volume-share-name $APP \
-    --azure-file-volume-mount-path /usr/local/apache2/htdocs/
-
-$CNAME=$( az container show \
-    -g $APP -n $APP --query "ipAddress.fqdn" -o tsv )
-
-echo "Please create a CNAME record: $CNAME"
+az container delete -g $APP -n $APP -y
