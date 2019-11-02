c3b707c56d3127045cf38dba8532f0480dcd19bc
maduma <maduma@pt.lu>
2019-11-02 17:02:07 +0100

    .

--- azure-up.sh
+++ azure-up.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -v
+
 APP=verctl
 LOCATION=westeurope
 
 # Create a resource group
+az group show --name $APP -o none 2>/dev/null || \
 az group create -l $LOCATION -n $APP
 
 # Create a storage account
+az storage acount show --name $APP -o none 2>/dev/null || \
 az storage account create \
     -g $APP \
     -l $LOCATION \
